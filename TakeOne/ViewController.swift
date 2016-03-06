b4686835a71eddd2c75fa8303b55a001114f3912
YuichiSawada <y_084743@yahoo.co.jp>
2016-03-06 17:25:32 +0900

    ログイン画面の画像の追加

--- TakeOne/ViewController.swift
+++ TakeOne/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //ログイン時のイメージ
+    @IBOutlet weak var loginImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let loginImage = UIImage(named: "logBg.png")
+        loginImageView.image = loginImage
     }
 
     override func didReceiveMemoryWarning() {
