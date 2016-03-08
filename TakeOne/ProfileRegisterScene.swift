17c2fe0787d6aa46276443bbd1b9c1077bb83491
YuichiSawada <y_084743@yahoo.co.jp>
2016-03-09 02:58:36 +0900

    プロフィールで畫像を追加

--- TakeOne/ProfileRegisterScene.swift
+++ TakeOne/ProfileRegisterScene.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 
-class ProfileRegisterScene: UIViewController {
+class ProfileRegisterScene: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var profileImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        profileImageView.contentMode = .ScaleAspectFit
     }
     
     override func didReceiveMemoryWarning() {
@@ -25,7 +28,53 @@ class ProfileRegisterScene: UIViewController {
         NSUserDefaults.standardUserDefaults().synchronize()
         /////////////////////////////////////////////////////////////
     }
-    @IBAction func onUploadPicture(sender: UIButton) {
+    
+    /**
+     ライブラリから写真を選択する
+     */
+    func pickImageFromLibrary() {
         
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary) {    //追記
+            
+            //写真ライブラリ(カメラロール)表示用のViewControllerを宣言しているという理解
+            let controller = UIImagePickerController()
+            
+            //おまじないという認識で今は良いと思う
+            controller.delegate = self
+            
+            //新しく宣言したViewControllerでカメラとカメラロールのどちらを表示するかを指定
+            //以下はカメラロールの例
+            //.Cameraを指定した場合はカメラを呼び出し(シミュレーター不可)
+            controller.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+            
+            //新たに追加したカメラロール表示ViewControllerをpresentViewControllerにする
+            self.presentViewController(controller, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func onUploadPicture(sender: UIButton) {
+        pickImageFromLibrary()
+    }
+    
+    /**
+     写真を選択した時に呼ばれる (swift2.0対応)
+     
+     :param: picker:おまじないという認識で今は良いと思う
+     :param: didFinishPickingMediaWithInfo:おまじないという認識で今は良いと思う
+     */
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo: [String: AnyObject]) {
+        
+        //このif条件はおまじないという認識で今は良いと思う
+        if didFinishPickingMediaWithInfo[UIImagePickerControllerOriginalImage] != nil {
+            
+            //didFinishPickingMediaWithInfo通して渡された情報(選択された画像情報が入っている？)をUIImageにCastする
+            //そしてそれを宣言済みのimageViewへ放り込む
+            profileImageView.image = didFinishPickingMediaWithInfo[UIImagePickerControllerOriginalImage] as? UIImage
+            
+            
+        }
+        
+        //写真選択後にカメラロール表示ViewControllerを引っ込める動作
+        picker.dismissViewControllerAnimated(true, completion: nil)
     }
 }
