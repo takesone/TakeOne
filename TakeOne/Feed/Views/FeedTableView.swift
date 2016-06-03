7530dbfad8d9bfd7e34ffcfb45574e46254dbe9c
Kusumoto Masahiro <a14dc057@dhw.ac.jp>
2016-06-03 14:42:00 +0900

    フィードのTableViewを作成&naviBarを隠しました

---
+++ TakeOne/Feed/Views/FeedTableView.swift
@@ -0,0 +1,13 @@
+//
+//  FeedTableView.swift
+//  TakeOne
+//
+//  Created by Kusumoto Masahiro on 2016/06/03.
+//  Copyright © 2016年 YuichiSawada. All rights reserved.
+//
+
+import UIKit
+
+class FeedTableView: UITableView {
+
+}
