124fcd1ed076acf0536cdc8fdb9b5ecf61463cb3
Kusumoto Masahiro <a14dc057@dhw.ac.jp>
2016-06-03 15:01:31 +0900

    とりあえずサンプルのTableViewを設置

--- TakeOne/Feed/Views/FeedTableView.swift
+++ TakeOne/Feed/Views/FeedTableView.swift
@@ -8,6 +8,57 @@
 
 import UIKit
 
-class FeedTableView: UITableView {
-
+class FeedTableView: UITableView, UITableViewDataSource, UITableViewDelegate {
+    
+    var showChatViewController: (()->Void)?
+    
+    // テストアイテム
+    let testItem = ["TEST1", "TEST2", "TEST3"]
+    
+    override func awakeFromNib() {
+        delegate = self
+        dataSource = self
+        
+        backgroundColor = UIColor.clearColor()
+        contentInset.bottom = 8
+        rowHeight = UITableViewAutomaticDimension
+        
+        registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath:NSIndexPath) {
+        if let showChatViewController = showChatViewController {
+            showChatViewController()
+        }
+    }
+    
+    // MARK: - UITableViewDataSource Methods -
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return testItem.count
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell")! as UITableViewCell
+        cell.textLabel?.text = "sample"
+        return cell
+    }
+    
+    // MARK: - UITableViewDelegate Methods -
+    
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
 }
